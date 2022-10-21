34a3c478fed9c4fc26aa1a7c0f1f86776224b896
영빈 <>
2022-10-21 20:20:40 +0900

    Gui calculator

--- calc-gui.js
+++ calc-gui.js
@@ -1,10 +1,65 @@
 const input = {'array' : []};
 input.getInput = function() {
     return this.array.join('');
-}
+};
+input.removeAll = function(value) {
+    this.array = [];
+    this.array.push(value);
+};
+
+input.isEmpty = function() {
+    return this.array.length === 0;
+};
+
+input.prepareCalculate = function() {
+    this.array = this.array.join('').split(' ');
+};
+
+input.getValue = function() {
+    var str = this.array.shift();
+    var n = Number(str);
+    return n;
+};
+
+input.getOperator = function() {
+    var op = this.array.shift();
+    if (op === "+" || op === "-" || op === "*" || op === "/") {
+        return op;
+    } else {
+        return '$';
+    }
+};
+
+const calculator = {};
+calculator.calculate = function(first, second, op) {
+    var ret;
+    switch(op) {
+        case "+":
+        ret = first + second;
+        break;
+        case "-":
+        ret = first - second;
+        break;
+        case "*":
+        ret = first * second;
+        break;
+        case "/":
+        ret = first / second;
+        break;
+        default:
+        return NaN;
+    }
+    return ret;
+};
 
 const output = {};
 output.text = document.getElementById('output');
+output.print = function(str) {
+    this.text.innerHTML = str;
+};
+output.display = function() {
+    this.text.innerHTML = input.getInput();
+};
 
 const clickNumbers = function(event) {
     var str = event.target.innerHTML;
@@ -17,80 +72,22 @@ const clickNumbers = function(event) {
     } else {
         input.array.push(str);
     }
-    if (input.array.length === 0) {
+    if (input.isEmpty()) {
         output.text.innerHTML = 'empty';
     } else {
-        output.text.innerHTML = input.getInput();
+        output.display();
     }
     console.log(input.getInput());
-}
+};
+
 const showResult = function(event) {
-    console.log("click others");
-    console.log(event.target.innerHTML);
-}
-
-// var input = {};
-
-//         input.init = function(str) {
-//             this.list = str.split(" ");
-//         };
-
-//         input.empty = function() {
-//             return this.list.length === 0;
-//         };
-
-//         input.getValue = function() {
-//             var str = this.list.shift();
-//             var n = Number(str);
-//             return n;
-//         };
-
-//         input.getOperator = function() {
-//             var op = this.list.shift();
-//             if (op === "+" || op === "-" || op === "*" || op === "/") {
-//                 return op;
-//             } else {
-//                 return '$';
-//             }
-//         };
-
-//         calculator = {};
-//         calculator.calculate = function(first, second, op) {
-//             var ret;
-//             switch(op) {
-//                 case "+":
-//                 ret = first + second;
-//                 break;
-//                 case "-":
-//                 ret = first - second;
-//                 break;
-//                 case "*":
-//                 ret = first * second;
-//                 break;
-//                 case "/":
-//                 ret = first / second;
-//                 break;
-//                 default:
-//                 return NaN;
-//             }
-//             return ret;
-//         };
-
-//         var output = {};
-//         output.out = document.getElementById('output');
-
-//         output.print = function(value) {
-//             this.out.innerHTML = '최종 결과값은 ' + value + '입니다.';
-//         };
-
-//         function calc() {
-//             var str = document.getElementById('input').value;
-//             input.init(str);
-//             var result = input.getValue();
-//             while (!input.empty()) {
-//                 var op = input.getOperator();
-//                 var second = input.getValue();
-//                 result = calculator.calculate(result, second, op);
-//             }
-//             output.print(result);
-//         };
+    input.prepareCalculate();
+    let result = input.getValue();
+    while (!input.isEmpty()) {
+        let op = input.getOperator();
+        let second = input.getValue();
+        result = calculator.calculate(result, second, op);
+    }
+    output.print(result);
+    input.removeAll(result);
+};
